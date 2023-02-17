1572a32db399b247e67d8c7254f78a039c42ca48
Chung Tran <bigearsenal@gmail.com>
2023-02-17 10:53:41 +0700

    Initial Commit

---
+++ RepositoryTests/RepositoryTestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RepositoryTestsApp.swift
+//  RepositoryTests
+//
+//  Created by Chung Tran on 17/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct RepositoryTestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
