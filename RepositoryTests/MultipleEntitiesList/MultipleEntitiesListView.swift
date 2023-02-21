ab99f465ee430f684045ddac2da1c8bfe5034de5
Chung Tran <bigearsenal@gmail.com>
2023-02-21 12:41:21 +0700

    refactor: ViewBuilder

--- RepositoryTests/MultipleEntitiesList/MultipleEntitiesListView.swift
+++ RepositoryTests/MultipleEntitiesList/MultipleEntitiesListView.swift
@@ -27,42 +27,63 @@ struct MultipleEntitiesListView: View {
         }
     }
     
-    // MARK: - Books
+    // MARK: - View Builders
 
     var books: some View {
         Group {
             switch booksViewModel.state {
             case .empty(let status):
-                Section(header: Text("Books")) {
-                    EmptyListGroup(status: status) {
-                        await booksViewModel.reload()
-                    }
-                }
-                    .task {
-                        await booksViewModel.reload()
-                    }
+                emptyBooks(status: status)
             case .nonEmpty(let loadMoreStatus):
-                ForEach(booksViewModel.sections, id: \.id) {section in
-                    Section(header: Text("Books " + section.name)) {
-                        ForEach(section.items) { book in
-                            Text(book.name)
+                nonEmptyBooks(loadMoreStatus: loadMoreStatus)
+            }
+        }
+    }
+    
+    var songs: some View {
+        Section(header: Text("Songs")) {
+            switch songsViewModel.state {
+            case .empty(let status):
+                emptySongs(status: status)
+            case .nonEmpty(let loadMoreStatus):
+                nonEmptySongs(loadMoreStatus: loadMoreStatus)
+            }
+        }
+    }
+    
+    // MARK: - Helpers
+
+    func emptyBooks(status: ListLoadingState.Status) -> some View {
+        Section(header: Text("Books")) {
+            EmptyListGroup(status: status) {
+                await booksViewModel.reload()
+            }
+        }
+            .task {
+                await booksViewModel.reload()
+            }
+    }
+    
+    func nonEmptyBooks(loadMoreStatus: ListLoadingState.LoadMoreStatus) -> some View {
+        ForEach(booksViewModel.sections, id: \.id) {section in
+            Section(header: Text("Books " + section.name)) {
+                ForEach(section.items) { book in
+                    Text(book.name)
+                }
+                switch loadMoreStatus {
+                case .loading:
+                    Button("Fetch more") {
+                        Task {
+                            await booksViewModel.fetchNext()
                         }
-                        switch loadMoreStatus {
-                        case .loading:
-                            Button("Fetch more") {
-                                Task {
-                                    await booksViewModel.fetchNext()
-                                }
-                            }
-                                
-                        case .reachedEndOfList:
-                            EmptyView()
-                        case .error:
-                            Button("Error fetching more item... Tap to try again") {
-                                Task {
-                                    await booksViewModel.fetchNext()
-                                }
-                            }
+                    }
+                        
+                case .reachedEndOfList:
+                    EmptyView()
+                case .error:
+                    Button("Error fetching more item... Tap to try again") {
+                        Task {
+                            await booksViewModel.fetchNext()
                         }
                     }
                 }
@@ -70,53 +91,48 @@ struct MultipleEntitiesListView: View {
         }
     }
     
-    var songs: some View {
-        Group {
-            switch songsViewModel.state {
-            case .empty(let status):
-                EmptyListGroup(status: status) {
-                    await songsViewModel.reload()
+    func emptySongs(status: ListLoadingState.Status) -> some View {
+        EmptyListGroup(status: status) {
+            await songsViewModel.reload()
+        }
+        .task {
+            await songsViewModel.reload()
+        }
+        .frame(height: 200)
+    }
+    
+    func nonEmptySongs(loadMoreStatus: ListLoadingState.LoadMoreStatus) -> some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            LazyHStack {
+                ForEach(songsViewModel.sections, id: \.id) {section in
+                    ForEach(section.items) { song in
+                        SongView(song: song)
+                    }
                 }
-                .task {
-                    await songsViewModel.reload()
-                }
-                .frame(height: 200)
-            case .nonEmpty(let loadMoreStatus):
-                Section(header: Text("Songs")) {
-                    ScrollView(.horizontal, showsIndicators: false) {
-                        LazyHStack {
-                            ForEach(songsViewModel.sections, id: \.id) {section in
-                                ForEach(section.items) { song in
-                                    SongView(song: song)
-                                }
-                            }
-                            
-                            switch loadMoreStatus {
-                            case .loading:
-                                LoadingView()
-                                    .frame(width: 120, height: 120)
-                                    .task {
-                                        await songsViewModel.fetchNext()
-                                    }
-                            case .reachedEndOfList:
-                                EmptyView()
-                            case .error:
-                                Text("Error")
-                                    .foregroundColor(.red)
-                                    .onTapGesture {
-                                        Task {
-                                            await songsViewModel.fetchNext()
-                                        }
-                                    }
+                
+                switch loadMoreStatus {
+                case .loading:
+                    LoadingView()
+                        .frame(width: 120, height: 120)
+                        .task {
+                            await songsViewModel.fetchNext()
+                        }
+                case .reachedEndOfList:
+                    EmptyView()
+                case .error:
+                    Text("Error")
+                        .foregroundColor(.red)
+                        .onTapGesture {
+                            Task {
+                                await songsViewModel.fetchNext()
                             }
                         }
-                    }
-                    .frame(height: 120)
-                    .listRowInsets(EdgeInsets())
-                    .listRowBackground(Color.clear)
                 }
             }
         }
+        .frame(height: 120)
+        .listRowInsets(EdgeInsets())
+        .listRowBackground(Color.clear)
     }
 }
 
